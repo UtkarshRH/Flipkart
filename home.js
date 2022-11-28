5b19b919e3a1d32c1850e1e7a18ccc130490eb4b
Utkarsh Harshe <utkarshharshe32@gmail.com>
2022-11-28 19:14:26 +0530

    c

---
+++ home.js
@@ -0,0 +1,18 @@
+function loadCopan() {
+
+    document.getElementById("copan").style.visibility = 'visibal';
+    document.getElementById('main').style.opacity = '0.7';
+    document.getElementById('cbtn').style.opacity = '0';
+
+}
+
+function closCopan() {
+    document.getElementById('copan').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1';
+}
+function hbtn() {
+    document.getElementById('cbtn').style.opacity = '1';
+}
+
+// geolocatio and wether
+
